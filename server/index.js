d698e10a2fa20a02b521c19da99c1bb2c3149f71
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-23 13:56:23 +0530

    Database connection and get and create post routes created

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+import {mongoURI} from './config/keys.js'
+
+const app = express()
+
+app.use('/posts' , postRoutes)
+
+app.use(bodyParser.json({ limit : "30mb" , extented : true}))
+app.use(bodyParser.urlencoded({ limit : "30mb" , extented : true}))
+app.use(cors())
+
+const CONNECTION_URL = mongoURI
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL , {useNewUrlParser : true , useUnifiedTopology : true})
+    .then(() => app.listen(PORT , () =>console.log(`Server Running On Port: ${PORT}`)))
+    .catch((err) => console.log(err.message))
