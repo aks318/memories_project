02fee7a5d7d8520078d57154b8f8121bf1237653
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-29 23:39:18 +0530

    added proxy to package.json in server side for api request

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ import {mongoURI} from './config/keys.js'
 
 const app = express()
 
-app.use('/posts' , postRoutes)
 
 app.use(bodyParser.json({ limit : "30mb" , extented : true}))
 app.use(bodyParser.urlencoded({ limit : "30mb" , extented : true}))
 app.use(cors())
+
+app.use('/posts' , postRoutes)
+
 
 const CONNECTION_URL = mongoURI
 const PORT = process.env.PORT || 5000
