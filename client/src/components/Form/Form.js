98b7f4cc5239e0203d0b6cde1a9866f8309569b8
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-10-01 16:18:24 +0530

    Additional fixes

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -53,7 +53,7 @@ const Form = ({currentId , setCurrentId}) => {
 
                 <TextField name = "tags" variant = "outlined"
                 label = "tags" fullWidth value = {postData.tags} 
-                onChange = {(e) =>setPostData({...postData , tags: e.target.value})} />
+                onChange = {(e) =>setPostData({...postData , tags: e.target.value.split(',')})} />
 
                 <div className = {classes.fileInput}>
                     <FileBase 
